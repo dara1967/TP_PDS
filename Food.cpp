74303f1359e26d8b5d0a285bbdd7488d893b2e04
dara1967 <dara1967ufmg@gmail.com>
2024-08-05 17:26:31 -0300

    Create Food.cpp

---
+++ Food.cpp
@@ -0,0 +1,4 @@
+#include "Food.hpp"
+
+Food::Food(std::string name, double carbs, double proteins, double fats, double calories)
+    : name(name), carbs(carbs), proteins(proteins), fats(fats), calories(calories) {}
