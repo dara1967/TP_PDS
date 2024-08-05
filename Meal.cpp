302287010c80de168793d788e3ec556c5ba6470f
dara1967 <dara1967ufmg@gmail.com>
2024-08-05 17:32:51 -0300

    Update Meal.cpp

--- Meal.cpp
+++ Meal.cpp
@@ -31,32 +31,3 @@ void Meal::calculate_food_quantities(double carbs, double proteins, double fats)
         std::cout << food.name << ": " << quantity << " grams\n";
     }
 }
-
-/*void Meal::print_options() {
-    std::cout << "Opções de carboidratos:\n";
-    for (size_t i = 0; i < carbs_options.size(); ++i) {
-        std::cout << i + 1 << ". " << carbs_options[i].name << ": " 
-                  << carbs_options[i].carbs << "g carbs, " 
-                  << carbs_options[i].proteins << "g proteins, " 
-                  << carbs_options[i].fats << "g fats, " 
-                  << carbs_options[i].calories << " kcal\n";
-    }
-
-    std::cout << "Opções de proteinas:\n";
-    for (size_t i = 0; i < proteins_options.size(); ++i) {
-        std::cout << i + 1 << ". " << proteins_options[i].name << ": " 
-                  << proteins_options[i].carbs << "g carbs, " 
-                  << proteins_options[i].proteins << "g proteins, " 
-                  << proteins_options[i].fats << "g fats, " 
-                  << proteins_options[i].calories << " kcal\n";
-    }
-
-    std::cout << "Opções de gorduras:\n";
-    for (size_t i = 0; i < fats_options.size(); ++i) {
-        std::cout << i + 1 << ". " << fats_options[i].name << ": " 
-                  << fats_options[i].carbs << "g carbs, " 
-                  << fats_options[i].proteins << "g proteins, " 
-                  << fats_options[i].fats << "g fats, " 
-                  << fats_options[i].calories << " kcal\n";
-    }
-}*/
