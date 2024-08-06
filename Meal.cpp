522fa3a067f100665f307cbf92093a865f348553
dara1967 <dara1967ufmg@gmail.com>
2024-08-06 10:25:39 -0300

    Update Meal.cpp

--- Meal.cpp
+++ Meal.cpp
@@ -13,21 +13,21 @@ void Meal::add_fat_option(Food food) {
     fats_options.push_back(food);
 }
 void Meal::calculate_food_quantities(double carbs, double proteins, double fats) {
-    std::cout << "Quantities for carbohydrate options:\n";
+    std::cout << "Quantidade de carboidratos:\n";
     for (const auto& food : carbs_options) {
         double quantity = (carbs / food.carbs) * 100;
-        std::cout << food.name << ": " << quantity << " grams\n";
+        std::cout << food.name << ": " << quantity << " gramas\n";
     }
 
-    std::cout << "Quantities for protein options:\n";
+    std::cout << "Quantidade de proteínas:\n";
     for (const auto& food : proteins_options) {
         double quantity = (proteins / food.proteins) * 100;
-        std::cout << food.name << ": " << quantity << " grams\n";
+        std::cout << food.name << ": " << quantity << " gramas\n";
     }
 
-    std::cout << "Quantities for fat options:\n";
+    std::cout << "Quantidade de gordura:\n";
     for (const auto& food : fats_options) {
         double quantity = (fats / food.fats) * 100;
-        std::cout << food.name << ": " << quantity << " grams\n";
+        std::cout << food.name << ": " << quantity << " gramas\n";
     }
 }
