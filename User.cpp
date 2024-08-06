69dec75bf574b46a683ec543325d28a38c61ece5
dara1967 <dara1967ufmg@gmail.com>
2024-08-06 10:28:05 -0300

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -6,18 +6,21 @@ User::User(double weight, double height, int age, char gender, std::string goal)
 double User::calculate_BM() {
     if (gender == 'M' || gender == 'm') {
         return 10 * weight + 6.25 * height - 5 * age + 5;
-    } else {
+    } else if (gender== 'F'|| gender =='f'){
         return 10 * weight + 6.25 * height - 5 * age - 161;
-    }
+    }else{
+        std::out << " Esse gênero não é existe nesse software";
+        return 0; //alterar, tentar substituir por loop de do...while
 }
 
 double User::calculate_daily_calories() {
     double bm = calculate_BM();
-    double daily_calories = bm * 1.55; // atividade moderada (alterar)
+    double daily_calories = bm * 1.55; // atividade moderada (tentar alterar para adcionar opção de leve e moderada)
     if (goal == "perder peso") {
         daily_calories -= 500;
     } else if (goal == "ganhar peso") {
         daily_calories += 500;
-    }
+    } else if(goal== "manter peso"){
     return daily_calories;
+    }
 }
